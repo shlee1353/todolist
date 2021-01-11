6272331672f6a076ac1dab6943bcf9fa38a65a2a
SEUNGHO LEE <shlee1353@gmail.com>
2021-01-12 00:47:39 +0900

    radio filter (#3)
    Co-authored-by: USER <user@AL01467022.local>

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -10,31 +10,61 @@ const TodoList = props => {
     const todoList = useSelector(state => state.todoList)
     const dispatch = useDispatch()
 
-    const [toggle, settoggle] = useState(false);
+    const [toggle, setToggle] = useState(false);
+    const [radioValue, setRadioValue] = useState("all");
 
     const handleToggle = () => {
-        settoggle(!toggle);
+        setToggle(!toggle);
     }
 
     const handleAddTodo = useCallback(({ id, title, description }) => {
         dispatch(addTodo({ id, title, description }));
-    }, [])
+    }, []);
 
     const handleDeleteTodo = useCallback( id => {
         dispatch(deleteTodo(id));
-    },[])
+    },[]);
 
     const handleModifyTodo = useCallback(({ id, allowance }) => {
         dispatch(modifyTodo({id, allowance}));
-    }, [])
+    }, []);
 
     const handleCompleteTodo = useCallback( id => {
         dispatch(completeTodo(id));
-    },[])
+    },[]);
+
+    const onRadioValueChange = useCallback( e => {
+        setRadioValue(e.target.value);
+    })
 
     return (
         <div>
-            <button onClick={handleToggle}>글쓰기</button>
+            <div className="user_control">
+                <button onClick={handleToggle}>글쓰기</button>
+                <div value="all">
+                    <label htmlFor="all">
+                        <input 
+                            type="radio"
+                            id="all"
+                            value="all"
+                            name="filter"
+                            checked={radioValue === "all"}
+                            onChange={onRadioValueChange}
+                        /> All
+                    </label>
+                    <label htmlFor="completed">
+                        <input 
+                            type="radio"
+                            id="completed"
+                            value="completed"
+                            name="filter"
+                            checked={radioValue === "completed"}
+                            onChange={onRadioValueChange}
+                        /> Completed
+                    </label>
+                </div>
+            </div>
+            {/* [DEV] 필터 기능 추가 */}
             {toggle && <TodoForm onClickAddTodo={handleAddTodo} />}
             {todoList?.map(todo => (
                 <Todo
